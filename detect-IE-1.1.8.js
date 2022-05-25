2e3030479cf39c49fba328ba8e51af15ba5fffbb
Ashraile <89170464+Ashraile@users.noreply.github.com>
2022-05-25 08:11:51 +0100

    Update detect-IE-1.1.8.js
    Expanded comments for those wishing to understand what is happening behind the scenes.

--- detect-IE-1.1.8.js
+++ detect-IE-1.1.8.js
@@ -1,10 +1,11 @@
 // Detect IE without User-agent hacks. | Supports minification. | MIT license. | Version 1.1.8 | https://github.com/Ashraile/detect-IE
+// Returns either `false` or an object containing the browser version and document mode of Internet Explorer that is running.
 
 window.isIE = (function(window, document, undefined) {
 
 	var envir, is_default_IE11 = !!( !document['currentScript'] && window.msCrypto ); // future proof window.msCrypto check by also checking for currentScript
 
-	var mapIE = { '5': 5, '5.5': 5.5, '5.6': 6, '5.7': 7, '5.8': 8, '9': 9, '10': 10, '11': 11 }; // IE browser versions
+	var mapIE = { '5': 5, '5.5': 5.5, '5.6': 6, '5.7': 7, '5.8': 8, '9': 9, '10': 10, '11': 11 }; // `@_jscript_version` mapped to IE browser versions.
 
 	var jscript_version = Number( new Function("/*@cc_on return @_jscript_version; @*\/")() ) || (is_default_IE11 ? 11 : undefined);
 
@@ -14,10 +15,16 @@ window.isIE = (function(window, document, undefined) {
 		Shoutout to https://stackoverflow.com/users/5807141/j-j for the overlooked comment on msCrypto:
 		https://stackoverflow.com/questions/21825157/internet-explorer-11-detection
 
-		Conditional compilation executes in IE only, and does not change with document mode.
-		By default, IE11 does not interpret conditional compilation, so `@_jscript_version` is `undefined`.
-		But if IE11's documentMode has been changed from the default, "is_default_IE11" will then return `false`,
-		and `@_jscript_version` will return "11". (Yep, textbook Microsoft)
+		Conditional compilation is a special comment syntax that executes in IE only. Regular browsers will interpret this syntax as a normal comment.
+		`@_jscript_version` is an internal jscript variable only accessible within conditional compilation, that does not change with IE document mode.
+		
+		By default, IE11** and other browsers do not interpret conditional compilation, so this check is as follows:
+		Number( new Function("")() ) -> Number(undefined) -> NaN
+		Instead of for example IE9:
+		Number( new Function("return '9'")() ) -> Number('9') -> 9
+		
+		** If IE11's documentMode has been changed from the default, `@_jscript_version` becomes exposed again, and returns "11". And
+		"is_default_IE11" will return `false`. (Yep, textbook Microsoft)
 
 		Values: IE6 / 7: "5.6" or "5.7", IE8: "5.8", IE9: "9", IE10: "10", IE11 alt-doc mode: "11", IE11 / NOT IE: `undefined`.
 	*/
@@ -31,7 +38,7 @@ window.isIE = (function(window, document, undefined) {
 
 	envir.browser = mapIE[ String(jscript_version) ] || jscript_version;
 
-	envir[envir.browser] = (envir.browser == envir.mode); // we want to make sure if we're screening for IE.x that its running as that with same doc mode
+	envir[envir.browser] = (envir.browser == envir.mode); // we want to make sure if we're screening for IE.x as IE.x that its running as that with same doc mode
 
 	return envir;
 
